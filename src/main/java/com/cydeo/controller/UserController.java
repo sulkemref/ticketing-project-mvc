e8e2549b7ff0c8eaa9c69b08db9c3c1a0874ce92
Kemail-terminal <sulkemref@gmail.com>
2023-03-11 12:35:42 -0600

    part-2-UserCreatePost

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -6,6 +6,7 @@ import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -28,4 +29,15 @@ public class UserController {
         model.addAttribute("users",userService.findAll());
         return "/user/create";
     }
+
+    @PostMapping("/create")
+    private String insertUser(@ModelAttribute("user") UserDTO user,Model model){
+        model.addAttribute("user", new UserDTO());
+        model.addAttribute("roles",roleService.findAll());
+        userService.save(user);
+        model.addAttribute("users",userService.findAll());
+        return "/user/create";
+    }
+
+
 }
