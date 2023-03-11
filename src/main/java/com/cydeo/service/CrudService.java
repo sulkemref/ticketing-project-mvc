aacd753921b4aec7c1c277f6bcd57aa0bbab0abb
Kemail-terminal <sulkemref@gmail.com>
2023-03-10 20:14:32 -0600

    part-0-done

---
+++ src/main/java/com/cydeo/service/CrudService.java
@@ -0,0 +1,12 @@
+package com.cydeo.service;
+
+import java.util.List;
+
+public interface CrudService <T,ID> {
+
+    T save(T object);
+    T findById(ID id);
+    List<T> findAll();
+    void deleteById(ID id);
+
+}
