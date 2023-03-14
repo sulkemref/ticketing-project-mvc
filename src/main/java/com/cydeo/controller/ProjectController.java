821f22f344e6e43637b566a13cf133384d1ff6ea
Kemail-terminal <sulkemref@gmail.com>
2023-03-14 16:16:29 -0500

    part-3-Project Service

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -1,6 +1,8 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.ProjectDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ProjectController {
 
     @GetMapping("/create")
-    public String createProject(){
+    public String createProject(Model model){
+        model.addAttribute("project",new ProjectDTO());
+        model.addAttribute("managers",)
         return "/project/create";
     }
 }
