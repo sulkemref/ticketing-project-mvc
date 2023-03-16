5cddaa6b37ff50a81082b5b6c9eea928583beae9
Kemail-terminal <sulkemref@gmail.com>
2023-03-16 15:39:19 -0500

    part-3-Project Create done

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -7,10 +7,7 @@ import com.cydeo.service.ProjectService;
 import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/project")
@@ -35,6 +32,12 @@ public class ProjectController {
     @PostMapping("/create")
     public String insertProject(@ModelAttribute("project") ProjectDTO project){
         projectService.save(project);
+        return "redirect:/project/create";
+    }
+
+    @GetMapping("delete/{projectCode}")
+    public String deleteProject(@PathVariable("projectCode")String projectCode){
+        projectService.deleteById(projectCode);
         return "redirect:/project/create";
     }
 }
