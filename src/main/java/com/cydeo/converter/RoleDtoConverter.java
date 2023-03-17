b3da323693dd70e64f6595a757cf4e60db9b9a5a
Kemail-terminal <sulkemref@gmail.com>
2023-03-17 15:18:33 -0500

    part-5-Project-Status

--- src/main/java/com/cydeo/converter/RoleDtoConverter.java
+++ src/main/java/com/cydeo/converter/RoleDtoConverter.java
@@ -8,11 +8,11 @@ import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationPropertiesBinding
-public class RoleDTOConverter implements Converter<String, RoleDTO> {
+public class RoleDtoConverter implements Converter<String, RoleDTO> {
 
     private final RoleService roleService;
 
-    public RoleDTOConverter(RoleService roleService) {
+    public RoleDtoConverter(RoleService roleService) {
         this.roleService = roleService;
     }
 
