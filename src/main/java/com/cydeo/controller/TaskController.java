f6a0cb52ba42cdc8a5fdf5d420f21dc075605da4
Kemail-terminal <sulkemref@gmail.com>
2023-03-17 11:18:28 -0500

    part-4-TaskService done

--- src/main/java/com/cydeo/controller/TaskController.java
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -1,18 +1,38 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.TaskDTO;
+import com.cydeo.dto.UserDTO;
+import com.cydeo.service.TaskService;
+import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/task")
 public class TaskController {
 
+    TaskService taskService;
+    UserService userService;
+
+    public TaskController(TaskService taskService, UserService userService) {
+        this.taskService = taskService;
+        this.userService = userService;
+    }
+
     @GetMapping("/create")
     public String taskCreate(Model model){
-
+        model.addAttribute("task", new TaskDTO());
         return "task/create";
+    }
+
+    @PostMapping("/create")
+    public String insertUser(TaskDTO task){
+        taskService.save(task);
+        return "redirect:/user/create";
     }
 
 }
