3d669b904e1d69dd14b9834fbf3d4560d962ef4b
Kemail-terminal <sulkemref@gmail.com>
2023-03-17 14:19:01 -0500

    part-5-TaskUpdate

--- src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -5,16 +5,23 @@ import com.cydeo.enums.Status;
 import com.cydeo.service.TaskService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class TaskServiceImpl extends AbstractMapService<TaskDTO,Long> implements TaskService {
     @Override
     public TaskDTO save(TaskDTO task) {
 
-        if(task.getTaskStatus()==null){
+        if(task.getTaskStatus() == null)
             task.setTaskStatus(Status.OPEN);
-        }
+
+        if(task.getAssignedDate() == null)
+            task.setAssignedDate(LocalDate.now());
+
+        if(task.getId() == null)
+            task.setId(UUID.randomUUID().getMostSignificantBits());
 
         return super.save(task.getId(), task);
     }
@@ -31,6 +38,10 @@ public class TaskServiceImpl extends AbstractMapService<TaskDTO,Long> implements
 
     @Override
     public void update(TaskDTO task) {
+
+        if(task.getTaskStatus() == null)
+            task.setTaskStatus(findById(task.getId()).getTaskStatus());
+
         super.update(task.getId(), task);
 
     }
